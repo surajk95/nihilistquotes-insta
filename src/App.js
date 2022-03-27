58daad20c982cd13590fbf9b3820b72ff2d145c7
surajk95 <surajkukati95@gmail.com>
2022-03-27 17:59:46 +0530

    automation done, but remote image html2canvas fail

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import imge from './img.jpg'
 import html2canvas from 'html2canvas'
 import { createApi } from "unsplash-js"
 import { unsplashKey } from './unsplashkey';
+import { useState, useEffect } from 'react';
+import { quotes } from './quotes'
 
 const api = createApi({
   // Don't forget to set your access token here!
@@ -11,32 +13,70 @@ const api = createApi({
 })
 
 function App() {
-  const saveImage = async () => {
-    const imageResult = await api.photos.getRandom({ query: "dark mountain", orientation: "landscape", count: 1 })
-    if(imageResult.type!=='success') {
-      console.log(`failed to fetch unsplash`)
-      const res = imageResult?.response[0]?.urls?.regular
-      console.log(`zzzimageurl`, res)
+  const [config, setConfig] = useState({shouldGenerate: true, index: 0})
+
+  // useEffect(() => {
+  //   const imageContainer = document.querySelector(`#unsplashImage`)
+  //   imageContainer.addEventListener(`load`, onImageLoad)
+
+  //   return (() => {
+  //     imageContainer.removeEventListener(`load`, onImageLoad)
+  //   })
+  // }, [])
+
+  const onImageLoad = async () => {
+    console.log(`onimageload`, config)
+    if(config.index===0)  {
+      setConfig(config => ({...config, index: 1}))
+      return
     }
-    //setPhotosResponse(result);
-    console.log(`zzzresponse`, imageResult)
+    if(!config.shouldGenerate) return
 
-
+    //change quote and trigger download
+    if(config.index===3) {
+      setConfig({...config, shouldGenerate: false})
+      alert(`finished downloading`)
+      return
+    }
+    const currentQuote = quotes[config.index]
+    setConfig({...config, index: config.index+1})
+    const quoteContainer = document.querySelector(`#text`)
+    quoteContainer.innerHTML = currentQuote
+    console.log(`zzz changed quote`, currentQuote)
+        
+    // const imageContainer = document.querySelector(`#unsplashImage`)
+    // console.log(`zzz image loaded`, imageContainer.src)
     const canvas = await html2canvas(document.querySelector("#app"))
     const dataURL = canvas.toDataURL()
+    console.log(`zzzgeneratedurl`, dataURL)
     let link = document.createElement("a")
-    link.download = "name"
+    link.download = "#nihilistquotes #quotes #nihilist #quote #love #art #feelings #sad #me #philosophy #app #thoughts #instagram"
     link.href = dataURL
     document.body.appendChild(link)
-    //link.click()
+    setTimeout(link.click(), 1000)
     document.body.removeChild(link)
+
+    //saveImage()
   }
+
+  const saveImage = async () => {
+    const imageResult = await api.photos.getRandom({ query: "dark mountain mountains hills landscape", orientation: "landscape", count: 1 })
+    if(imageResult.type!=='success') {
+      console.log(`failed to fetch unsplash`)
+    }
+    const res = imageResult?.response[0]?.urls?.regular
+    console.log(`zzzimageurl`, res)
+    const imageContainer = document.querySelector(`#unsplashImage`)
+    imageContainer.src = res
+  }
+
+
   
   return (
     <>
       <div className="app" id="app">
-        <img src={imge} className="img" alt="preview" />
-        <div className="text">there is only one truth; rest are conjectures.</div>
+        <img src={imge} className="img" id="unsplashImage" alt="preview" onLoad={onImageLoad} />
+        <div className="text" id="text">initial quote. this won't download.</div>
       </div>
       <button onClick={saveImage}>save</button>
     </>
